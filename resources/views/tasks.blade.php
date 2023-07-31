9c4589d171f9b04926079c2d5aeef274ea0612f4
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2023-07-31 16:11:01 +0100

    Updated Tasks flow 3737398989

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -12,7 +12,7 @@
                     <thead>
                         <tr style="color: ">
                             <th>Title</th>
-                            <th style="width: 30%">Details</th>
+                            <th style="width: 30% !important;">Details</th>
                             <th>Member Info</th>
                             <th>Date</th>
                             <th>Status</th>
